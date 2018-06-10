8e481c282729ccce2d897a21095ed02313cbbb82
unknown <neel.vikmani@gmail.com>
2018-06-11 00:52:40 +0530

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Resources\Product\ProductResource;
+use App\Model\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
+    public function index(){
+        return Product::all();
+    }
+    public function show(Product $product){
+        //return $product;
+        return new ProductResource($product);
+    }
 }
---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'name' => $this->name,
+           'price' => $this->price,
+           'stock' => $this->stock
+        ];
+    }
+}
