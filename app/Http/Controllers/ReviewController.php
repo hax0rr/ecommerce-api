da4329ee49fccdcacaa581c49e1fe0bdacb1d9f5
unknown <neel.vikmani@gmail.com>
2018-07-01 16:24:29 +0530

    Review Created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
+use App\Http\Requests\ReviewRequest;
 use Illuminate\Http\Request;
 use App\Http\Resources\ReviewResource;
+use Symfony\Component\HttpFoundation\Response;
+
 class ReviewController extends Controller
 {   
     public function index(Product $product){
         
         return ReviewResource::collection($product->reviews);
     }
-    
+    public function store(ReviewRequest $request,Product $product){
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+    protected $fillable = [
+        'customer','review'    
+    ];
     public function product(){
         return $this->belongsTo(Product::class);
     }
